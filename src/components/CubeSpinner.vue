b026372c9e1a3a31c47eebd7e61bfbe60a7e748b
cherylac <38063622+cherylac@users.noreply.github.com>
2018-11-25 22:46:25 -0800

    pushing project

--- src/components/CubeSpinner.vue
+++ src/components/CubeSpinner.vue
@@ -42,7 +42,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #333;
+  background-color: #9d28cc;
   -webkit-animation: sk-foldCubeAngle 2.4s infinite linear both;
           animation: sk-foldCubeAngle 2.4s infinite linear both;
   -webkit-transform-origin: 100% 100%;
